ca9d3dc767a59acbc3f97c79726a80e997c75e37
Chuck Lee <chuckli0706@gmail.com>
2019-04-11 11:22:46 +0800

    First commit

---
+++ reveal.js
@@ -0,0 +1,9 @@
+var ndPaywall = document.getElementsByClassName('ndPaywall');
+for (var i = 0; i < ndPaywall.length; i++) {
+    ndPaywall[i].style.display = 'none';
+}
+
+var ndArticle_margin = document.getElementsByClassName('ndArticle_margin');
+for (var i = 0; i < ndArticle_margin.length; i++) {
+    ndArticle_margin[i].style.display = '';
+}
