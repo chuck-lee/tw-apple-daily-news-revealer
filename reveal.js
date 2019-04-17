f18ea80cf394d930d3084fbdf1088c6229047805
Chuck Lee <ChuckSC.Lee@moxa.com>
2019-04-17 14:55:26 +0800

    Issue #5: 02. Preserve nodes containing contents when the node is being deleted

--- reveal.js
+++ reveal.js
@@ -1,3 +1,16 @@
+var PRESERVE_IDS = [
+    'playerVideo',
+    'video_player'
+];
+
+var PRESERVE_CLASSES = [
+    'ndAritcle_headPic',
+    'ndArticle_margin',
+    'mediabox'
+];
+
+var PRESERVED_DOM_ELEMENTS = {};
+
 function reveal_content_mobile()
 {
     var ndgPayway = document.getElementsByClassName('ndgPayway');
@@ -49,3 +62,39 @@ document.addEventListener('readystatechange', (event) => {
         reveal_content();
     }
 })
+
+function preserve_node(node)
+{
+    for (var i = 0; i < PRESERVE_IDS.length; i++) {
+        var preserve_id = PRESERVE_IDS[i];
+        if (node.id == preserve_id) {
+            node.style.display = '';
+            PRESERVED_DOM_ELEMENTS[preserve_id] = node.cloneNode(true);
+            return;
+        }
+    }
+
+    for (var i = 0; i < PRESERVE_CLASSES.length; i++) {
+        var preserve_class = PRESERVE_CLASSES[i];
+        if (node.classList.contains(preserve_class)) {
+            node.style.display = '';
+            PRESERVED_DOM_ELEMENTS[preserve_class] = node.cloneNode(true);
+            return;
+        }
+    }
+}
+
+var config = {childList: true, subtree: true};
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type != 'childList') {
+            return;
+        }
+
+        // Preserve content node
+        for (var i = 0; i < mutation.removedNodes.length; i++) {
+            preserve_node(mutation.removedNodes[i]);
+        }
+    });
+})
+observer.observe(document, config);
